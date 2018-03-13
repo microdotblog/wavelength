f9c2736226865b6f97acd5bfa01e097abc5f8c8f
Jonathan Hays <jonathanhays@Jonathans-MacBook-Pro.local>
2018-03-12 19:55:02 -0700

    Initial project creation

---
+++ Lumos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lumos
+//
+//  Created by Jonathan Hays on 3/12/18.
+//  Copyright © 2018 Jonathan Hays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lumos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lumos
+//
+//  Created by Jonathan Hays on 3/12/18.
+//  Copyright © 2018 Jonathan Hays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
